ef80136da39b8f953f5d8aee17c5500c06f365cc
vm1-23 <173900329+vm1-23@users.noreply.github.com>
2026-01-01 23:16:43 +0530

    Code formatted using clang-format

--- include/OrderBook.h
+++ include/OrderBook.h
@@ -1,76 +1,88 @@
-#include<bits/stdc++.h>
 #include "DataStructures.h"
+#include <bits/stdc++.h>
 using namespace std;
 
+class OrderBook {
+private:
+  friend class MatchingEngine;
+  OrderPool order_pool;
+  PriceLevelPool price_level_pool;
+  TradeEventPool trade_event_pool;
+  unordered_map<OrderId, Order *> order_index;
+  map<Price, PriceLevel *> buy_book;
+  map<Price, PriceLevel *> sell_book;
+  PriceLevel *best_bid;
+  PriceLevel *best_ask;
 
+public:
+  OrderBook() : best_bid(nullptr), best_ask(nullptr) {}
 
-class OrderBook{
-    private:
-    friend class MatchingEngine;
-    OrderPool order_pool;
-    PriceLevelPool price_level_pool;
-    TradeEventPool trade_event_pool;
-    unordered_map<OrderId,Order*> order_index;
-    map<Price,PriceLevel*> buy_book;
-    map<Price,PriceLevel*> sell_book;
-    PriceLevel* best_bid;
-    PriceLevel* best_ask;
+private:
+  void insertOrder(Order *o) {
+    PriceLevel *level = getOrCreatePriceLevel(o->side, o->price);
+    fifoPush(level, o);
+    order_index[o->order_id] = o;
+    if (o->side == Side::BUY) {
+      best_bid = buy_book.rbegin()->second;
+    } else {
+      best_ask = sell_book.begin()->second;
+    }
+  }
 
-    public:
-    OrderBook() : best_bid(nullptr),best_ask(nullptr){}
+  void removeOrder(Order *o) {
+    PriceLevel *level = getOrCreatePriceLevel(o->side, o->price);
+    fifoRemove(level, o);
+    removePriceLevelIfEmpty(o->side, level->price);
+    order_index.erase(o->order_id);
+    if (o->side == Side::BUY) {
+      if (buy_book.empty()) {
+        best_bid = nullptr;
+      } else {
+        best_bid = buy_book.rbegin()->second;
+      }
+    } else {
+      if (sell_book.empty()) {
+        best_ask = nullptr;
+      } else {
+        best_ask = sell_book.begin()->second;
+      }
+    }
+    order_pool.deallocate(o);
+  }
 
-    private:
-    void insertOrder(Order* o){
-        PriceLevel* level = getOrCreatePriceLevel(o->side,o->price);
-        fifoPush(level,o);
-        order_index[o->order_id] = o;
-        if(o->side == Side::BUY){best_bid = buy_book.rbegin()->second;}
-        else{best_ask = sell_book.begin()->second;}
+  PriceLevel *getOrCreatePriceLevel(Side side, Price price) {
+    auto &book = (side == Side::BUY) ? buy_book : sell_book;
+
+    auto lvl_it = book.find(price);
+    if (lvl_it != book.end()) {
+      return lvl_it->second;
     }
 
-    void removeOrder(Order* o){
-        PriceLevel* level = getOrCreatePriceLevel(o->side,o->price);
-        fifoRemove(level,o);
-        removePriceLevelIfEmpty(o->side,level->price);
-        order_index.erase(o->order_id);
-        if(o->side == Side::BUY){
-            if(buy_book.empty()){best_bid = nullptr;}
-            else{best_bid = buy_book.rbegin()->second;}
-        }
-        else{
-            if(sell_book.empty()){best_ask = nullptr;}
-            else{best_ask = sell_book.begin()->second;}
-        }
-        order_pool.deallocate(o);
-    }
+    PriceLevel *level = price_level_pool.allocate();
+    level->price = price;
+    book[price] = level;
+    return level;
+  }
 
-    PriceLevel* getOrCreatePriceLevel(Side side,Price price){
-        auto& book = (side == Side::BUY) ? buy_book : sell_book;
-        
-        auto lvl_it = book.find(price);
-        if(lvl_it != book.end()){return lvl_it->second;}
-        
-        PriceLevel* level = price_level_pool.allocate();
-        level->price = price;
-        book[price] = level;
-        return level;
-    }
+  void removePriceLevelIfEmpty(Side side, Price price) {
+    auto &book = (side == Side::BUY) ? buy_book : sell_book;
 
-    void removePriceLevelIfEmpty(Side side,Price price){
-        auto& book = (side == Side::BUY) ? buy_book : sell_book;
-
-        auto lvl_it = book.find(price);
-        if(lvl_it == book.end()){return;}
-        PriceLevel* level = lvl_it->second;
-        if(empty(level)){
-            book.erase(lvl_it);
-            price_level_pool.deallocate(level);
-        }
+    auto lvl_it = book.find(price);
+    if (lvl_it == book.end()) {
+      return;
     }
-
-    Order* findOrder(OrderId order_id){
-        auto it = order_index.find(order_id);
-        if(it == order_index.end()){return nullptr;}
-        return it->second;
+    PriceLevel *level = lvl_it->second;
+    if (empty(level)) {
+      book.erase(lvl_it);
+      price_level_pool.deallocate(level);
     }
+  }
+
+  Order *findOrder(OrderId order_id) {
+    auto it = order_index.find(order_id);
+    if (it == order_index.end()) {
+      return nullptr;
+    }
+    return it->second;
+  }
 };
