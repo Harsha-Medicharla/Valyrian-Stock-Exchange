a4c470718af8fec8742cabbaa8cbfabc1f075a18
vm1-23 <173900329+vm1-23@users.noreply.github.com>
2026-01-04 18:35:35 +0530

    Stop tracking ignored files

--- build.sh
+++
@@ -1,9 +0,0 @@
-chmod +x build.sh
-rm -rf build
-mkdir build
-cd build
-cmake ..
-make -j$(nproc)
-cd ..
-./matching_engine
-
--- build/CMakeFiles/art.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for art.
-# This may be replaced when dependencies are built.
